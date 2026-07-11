f3f1669829b1f07125239253bce7bcd79e9d0ad8
Soumika Sahoo <sahoosoumika247@gmail.com>
2026-07-11 23:08:57 +0530

    Time: 5 ms (80.71%) | Memory: 45 MB (43.45%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,45 +1,38 @@
 class Solution {
-    public int findTargetSumWays(int[]a, int target) {
-int sum=0;
-for(int x:a) sum=sum+x;
-if (Math.abs(target) > sum)
-    return 0;
-   if(sum+target<0) return 0;
-   if((sum+target)%2!=0) return 0;
-sum=(sum+target)/2;
-        int n = a.length;
-        int[][] dp = new int[n][sum + 1];
+    public int findTargetSumWays(int[] nums, int target) {
 
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        int n = nums.length;
 
-        return fun(a, n, n - 1, sum, dp);
-    }
+        int sum = 0;
+        for (int x : nums)
+            sum += x;
 
-    static int fun(int[] a, int n, int i, int sum, int[][] dp) {
-
-        if (i == 0) {
-            if (sum == 0 && a[0] == 0)
-                return 2;
-
-            if (sum == 0 || sum == a[0])
-                return 1;
-
+        if (Math.abs(target) > sum)
             return 0;
+
+        if ((sum + target) % 2 != 0)
+            return 0;
+
+        sum = (sum + target) / 2;
+
+        int[][] dp = new int[n + 1][sum + 1];
+
+        dp[n][0] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int s = 0; s <= sum; s++) {
+
+                if (nums[i] > s) {
+                    dp[i][s] = dp[i + 1][s];
+                } else {
+                    int c1 = dp[i + 1][s - nums[i]];
+                    int c2 = dp[i + 1][s];
+
+                    dp[i][s] = c1 + c2;
+                }
+            }
         }
 
-        if (dp[i][sum] != -1)
-            return dp[i][sum];
-
-        int c1 = 0;
-
-        if (a[i] <= sum)
-            c1 = fun(a, n, i - 1, sum - a[i], dp);
-
-        int c2 = fun(a, n, i - 1, sum, dp);
-
-        return dp[i][sum] = c1 + c2;
+        return dp[0][sum];
     }
 }
-    
